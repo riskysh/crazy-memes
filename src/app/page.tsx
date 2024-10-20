6fde0f6fdbd44cda12cf268f77e35a79b215a2cd
Risky <notriskysh@gmail.com>
2024-10-20 09:56:18 +0530

    configured drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,4 +4,7 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   redirect('/search?q=')
+  return (
+    <h1 className="text-8xl font-bold text-center mt-20">Redirecting...</h1>
+  )
 }
